da3743ccefbc2b7786e70df307a9f7bd88e95a50
FahmiAndriana <94207375+FahmiAndriana@users.noreply.github.com>
2021-12-22 22:06:32 +0700

    Add files via upload

---
+++ Fahmi.js
@@ -0,0 +1,15 @@
+
+       
+            class people {
+                constructor (nama, tempat, skill){
+                    this.nama = nama;
+                    this.tempat = tempat;
+                    this.skill = skill
+                  }
+            }
+
+            const fahmi = new people("Fahmi Andriana", "Garut", "Bermain Bola");
+            console.log(fahmi.nama)
+            console.log(fahmi.tempat)
+            console.log(fahmi.skill)
+
